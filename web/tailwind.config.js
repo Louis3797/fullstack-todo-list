5301fb0ca5be112c6e1521d3ac74dc662872d583
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-15 21:48:12 +0100

    cleaned web folder

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      primary: {
+        DEFAULT: "#DBE2EF",
+        600: "#c5cbd7",
+      },
+      secondary: "#3F72AF",
+      accent: "#112D4E",
+      white: "#ffffff",
+    },
     extend: {},
   },
   plugins: [],
