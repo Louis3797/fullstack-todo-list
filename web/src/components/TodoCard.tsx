467f0cc22b7abf3a780e69720349168f9331e8b5
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-19 18:27:26 +0100

    added checkbox and icons

--- web/src/components/TodoCard.tsx
+++ web/src/components/TodoCard.tsx
@@ -1,29 +1,46 @@
-import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import Moment from "react-moment";
 import { Todo } from "types";
 import { instance } from "utils/http";
+import { BsFillTrashFill } from "react-icons/bs";
 
 const TodoCard: React.FC<Todo> = ({ id, text, status, created_at }) => {
+  const [done, setDone] = useState<boolean>(status == true ? true : false);
+
+  function handleUpdate() {
+    const updatedTodo: Todo = {
+      id: id,
+      text: text,
+      status: !done,
+      created_at: created_at,
+    };
+    instance.patch(`todo/${id}`, JSON.stringify(updatedTodo));
+  }
   function deleteTodo() {
     instance.delete(`delete-todo/${id}`).then((res) => console.log(res));
   }
 
   return (
-    <div className="flex flex-col bg-primary w-full p-3 rounded-md">
+    <div className="flex flex-col bg-primary w-full p-3 rounded-md shadow-lg duration-500 hover:-translate-y-1 ease-in-out">
       <div className="flex flex-row justify-between items-center mb-4">
-        <p className="font-semibold text-accent text-lg">{text}</p>
-        <div className="flex flex-row">
-          <button className="bg-primary-600 rounded-md px-2 py-1 mr-2">
-            Update
-          </button>
-          <button
-            className="bg-primary-600 rounded-md px-2 py-1"
-            onClick={deleteTodo}
-          >
-            Delete
-          </button>
+        <div className="flex flex-row justify-center items-center space-x-3">
+          <input
+            type="checkbox"
+            checked={done}
+            onChange={() => {
+              setDone(!done);
+              handleUpdate();
+            }}
+            className="checked:bg-secondary"
+          />
+          <p className="font-semibold text-accent text-lg">{text}</p>
         </div>
+        <button
+          className=" rounded-md px-2 py-1 hover:text-red-400 duration-500 ease-in-out"
+          onClick={deleteTodo}
+        >
+          <BsFillTrashFill />
+        </button>
       </div>
       <div className="flex flex-row justify-between">
         <p className="text-sm font-semibold text-accent">
